5d7a342d2fde2b070cba1dc4bb8f37ef2d6718c8
Ricky Carlo Piencenaves <rcp@Rickys-Laptop.local>
2026-01-14 21:21:44 +0800

    Enabled WhiteNoise

--- scan_in/settings.py
+++ scan_in/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+WHITENOISE_USE_FINDERS = True
 
 
 JAZZMIN_SETTINGS = {
